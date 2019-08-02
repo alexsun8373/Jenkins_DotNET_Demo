c55e29bf89e91286aa0ab154db6ae60eb55dd9a3
Hao Sun <sunhao@ter.teradyne.com>
2019-08-02 23:09:17 +0800

    注释说明

---
+++ BasicMathsTest/BasicMathsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BasicMath;
+
+namespace BasicMathsTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+        [Test]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [Test]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [Test]
+        public void Test_DivideMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [Test]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
+//test
---
+++ BasicMathsTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicMath;
+using System.Text;
+using System.IO;
+
+namespace BasicMathsApp
+{
+    class Program
+    {
+        BasicMaths BM;
+        static void Main(string[] args)
+        {
+            BasicMaths bm = new BasicMaths();
+            string strWriteFilePath = @"WriteLog.txt";
+            StreamWriter swWriteFile = File.CreateText(strWriteFilePath);
+
+            for (int i = 0; i < 1000; i++) {
+
+                double res1 = bm.Add(i, 1);
+                double res2 = bm.Substract(i, 1);
+                double res3 = bm.divide(i, 2);
+                double res4 = bm.Multiply(i, 2);
+                Console.WriteLine(res1.ToString() + ' ' + res2.ToString() + ' ' + res3.ToString() + ' ' + res4.ToString() + ' ');
+                swWriteFile.WriteLine(res1.ToString() + ' ' + res2.ToString() + ' ' + res3.ToString() + ' ' + res4.ToString() + ' '); //写入读取的每行数据
+                //Console.WriteLine(res2.ToString() + ' ');
+                //Console.WriteLine(res3.ToString() + ' ');
+                //Console.WriteLine(res4.ToString() + ' ' + '\n');
+                System.Threading.Thread.Sleep(2000);
+            }
+           
+        }
+    }
+}
